c20abb152fdfe92518197192abb678fe33799656
eugenrecaj <geni.recaj11@gmail.com>
2024-02-02 12:04:23 +0100

    fix

--- src/api/socket.ts
+++ src/api/socket.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 export const socket = io(
   process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8080/'
+    ? 'https://coral-app-o6hgm.ondigitalocean.app/'
     : 'https://bonvue-backend-fceik.ondigitalocean.app/',
   {
     autoConnect: false,
